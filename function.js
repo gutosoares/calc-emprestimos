c50f7c375e41287001e987290a5b9a033feba648
Augusto Soares <gutosoares@outlook.com>
2015-04-04 19:06:17 -0300

    Funções incrementais

--- function.js
+++ function.js
@@ -13,4 +13,63 @@ function calculate() {
     var principal = parseFloat(amount.value);
     var interest = parseFloat(apr.value) / 100 / 12;
     var payments = parseFloat(years.value) * 12;
+
+    //Calcula o valor do pagamento mensal
+    var x = Math.pow(1 + interest, payments);
+    var monthly = (principal * x * interest) / (x - 1);
+
+    //Valores corretos
+    if(isFinite(monthly)) {
+        //Preenche os campos de saída, arredondando para 2 casas decimais
+        payment.innerHTML = monthly.toFixed(2);
+        total.innerHTML = (monthly * payments).toFixed(2);
+        totalinterest.innerHTML = ((monthly * payments) - principal).toFixed(2);
+
+        //Salva a entrada do usuário para ser usada posteriormente
+        save(amount.value, apr.value, years.value, zipcode.value);
+
+        try {
+            getLenders(amount.value, apr.value, years.value, zipcode.value);
+        }
+
+        catch(e) {
+            chart(principal, interest, monthly, payments);
+        }
+    }
+
+    //Valores incorretos
+    else {
+        payment.innerHTML = "";
+        total.innerHTML = "";
+        totalinterest.innerHTML = "";
+        chart();
+    }
+
+    //Salva a entrada do usuário. Esse recurso não ira funcionar em alguns navegadores
+    function save(amount, apr, years, zipcode) {
+        if(window.localStorage) {
+            localStorage.loan_amount = amount;
+            localStorage.loan_apr = apr;
+            localStorage.loan_years = years;
+            localStorage.loan_zipcode = zipcode;
+        }
+    }
+
+    //Tenta restaurar os campos de entrada automaticamente quando o documento é carregado pela primeira vez
+    window.onload = function() {
+        if(window.localStorage && localStorage.loan_amount) {
+            document.getElementById("amount").value = localStorage.loan_amount;
+            document.getElementById("apr").value = localStorage.loan_apr;
+            document.getElementById("years").value = localStorage.loan_years;
+            document.getElementById("zipcode").value = localStorage.loan_zipcode;
+        }
+    };
+
+
+
+
+
+
+
+
 }
