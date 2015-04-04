4d3c53c512a05879767df7549068e50deaefbd84
Augusto Soares <gutosoares@outlook.com>
2015-04-04 19:56:11 -0300

    Termino da edição

--- function.js
+++ function.js
@@ -65,11 +65,109 @@ function calculate() {
         }
     };
 
+    //Retorna uma lista de links para financeiras locais
+    function getLenders(amount, apr, years, zipcode) {
+        if(!window.XMLHttpRequest) return;
 
+        var ad = document.getElementById("lenders");
 
+        if(!ad) return;
 
+        var url = "getLenders.php" + "?amt=" + encodeURIComponent(amount) + "?apr=" + encodeURIComponent(apr) + "?yrs=" + encodeURIComponent(years) + "?zip=" + encodeURIComponent(zipcode);
+        var req = new XMLHttpRequest();
+        req.open("GET", url);
+        req.sen(null);
 
+        req.onreadystatechange = function() {
+            if(req.readyState == 4 && req.status == 200) {
+                var response = req.responseText;
+                var lenders = JSON.parse(response);
 
+                var list = "";
+                for(var i = 0; i < lenders.length; i++) {
+                    list += "<li><a href='"+ lenders[i].url +"'>" + lenders[i].name + "</a>";
+                }
 
+                ad.innerHTML = "<ul>" + list + "</ul>";
+            }
+        }
+    }
 
+    //Faz o gráfico do saldo devedor mensal, dos juros e do capital em um elemento <canvas> da HTML
+    function chart(principal, interest, monthly, payments) {
+        var graph = document.getElementById("graph");
+        graph.width = graph.width;
+
+        //Se chamamos sem argumentos ou se o navegador não suporta elementos gráficos, basta retornar
+        if(arguments.length == 0 || graph.getContext) return;
+
+        var g = graph.getContext("2d");
+        var width = graph.width, height = graph.height;
+
+        //Funções que convertem números de pagementos e valores monetários em pixels
+        function paymentToX(n) { return n * width/payments; }
+        function amountToY(a) { return height - (a * height / (monthly * payments * 1.05)); }
+
+        g.moveTo(paymentToX(0), amountToY(0));
+        g.lineTo(paymentToX(payments), amountToY(monthly* payments));
+        g.lineTo(paymentToX(payments), amountToY(0));
+
+        g.closePath();
+        g.fillStyle = '#f88';    //vemelho-claro
+        g.fill();    //preenche o triângulo
+        g.font = "bold 12px sans-serif";    //define a fonte
+        g.fillText("Total Interest Payments", 20,20);
+
+        var equity = 0;
+        g.beginPath();
+        g.moveTo(paymentToX(0), amountToY(0));
+
+        for(var p = 1; p <= payments; p++) {
+            var thisMonthInterest = (principal-equity) * interest;
+            equity += (monthly - thisMonthInterest);
+            g.lineTo(paymentToX(p), amountToY(equity));
+        }
+
+        g.lineTo(paymentToX(payments), amountToY(0));
+        g.closePath();
+        g.fillStyle = "green";
+        g.fill();    //preenche o triângulo
+        g.fillText("Total Equity", 20,35);
+
+        var bal = principal;
+        g.beginPath();
+        g.moveTo(paymentToX(0), amountToY(bal));
+        for(var p = 1; p <= payments; p++) {
+            var thisMonthInterest = bal*interest;
+            bal -= (monthly - thisMonthInterest);
+            g.lineTo(paymentToX(p), amountToY(bal));
+        }
+
+        g.lineWidth = 3;
+        g.stroke();
+        g.fillStyle = "black";
+        g.fillText("Loan Balance", 20,50);
+
+        g.textAlign="center";
+
+        var y = amountToY(0);
+        for(var year=1; year*12 <= payments; year++) {
+            var x = paymentToX(year*12);
+            g.fillRect(x-0.5, y-3,1,3);
+            if(year == 1)
+                g.fillText("Year", x, y-5);
+            if(year % 5 == 0 && year*12 !== payments)
+                g.fillText(String(year), x, y-5);
+        }
+
+        g.textAlign="right";
+        g.textBaseline = "middle";
+        var ticks = [monthly*payments, principal];
+        var rightEdge = paymentToX(payments);
+        for(var i = 0; i < ticks.length; i++) {
+            var y = amountToY(ticks[i]);
+            g.fillRect(rightEdge-3, y-0.5, 3, 1);
+            g.fillText(String(ticks[i].toFixed(0)), rightEdge-5, y);
+        }
+    }
 }
